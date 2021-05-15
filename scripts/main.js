2c4f0c8da61a4d39a315ccf3c27d43fd788ed67f
JadeD99 <79310337+JadeD99@users.noreply.github.com>
2021-05-15 10:31:20 +0200

    js

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,12 @@
 
 // setInterval(spin, 16);
 
-
 myOtherBox.addEventListener('mouseenter', function(){
-	rotationSpeed = 0.01;
+	rotationSpeed = 0.03;
 	console.log('mouse enter');
-		
 });
+
 myOtherBox.addEventListener('mouseleave', function(){
-	rotationSpeed = 0.001;
-	console.log('left');
+	rotationSpeed = 0.01;
+	console.log('mouse leave');
 });
